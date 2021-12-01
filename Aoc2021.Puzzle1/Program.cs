404c8a4aa56fb40a0de9e9631a3f9c5f7a937e7e
nanocortex <regnartsd@gmail.com>
2021-12-01 20:39:54 +0100

    Add solution for puzzle 1

---
+++ Aoc2021.Puzzle1/Program.cs
@@ -0,0 +1,8 @@
+﻿using Aoc2021.Puzzle1;
+using Aoc2021.Shared;
+
+var numbers = Utils.GetInputIntegers();
+var answerCalculator = new AnswerCalculator();
+
+Console.WriteLine($"Solution 1: {answerCalculator.Calculate1(numbers)}");
+Console.WriteLine($"Solution 2: {answerCalculator.Calculate2(numbers)}");
